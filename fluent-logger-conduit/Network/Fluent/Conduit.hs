300e3b579886d0719d734dd9f2243f6971e7c18d
notogawa <n.ohkawa@gmail.com>
2013-03-31 20:21:14 +0900

    Needs RankNTypes

--- fluent-logger-conduit/Network/Fluent/Conduit.hs
+++ fluent-logger-conduit/Network/Fluent/Conduit.hs
@@ -13,7 +13,7 @@
 -- See the License for the specific language governing permissions and
 -- limitations under the License.
 --
-
+{-# LANGUAGE RankNTypes #-}
 -- | Fluent Logger with Conduit Interface
 module Network.Fluent.Conduit
     ( -- * Sinks
